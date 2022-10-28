6c39eadd3602d016950b9e3f28566804396baecb
DongHyun Jung <jdh9490@gmail.com>
2022-10-28 22:14:37 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const sortBtn = document.getElementById('sort');
+const showBtn = document.getElementById('show');
+const calculateBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+
+// fetch random user and add money
+async function getRandomUser() {
+  const res = await fetch(`https://randomuser.me/api`);
+  const data = await res.json();
+  const user = data.results[0];
+  // Create new user obj
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    age: user.dob.age,
+    location: `${user.location.country}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+
+  addData(newUser);
+}
+
+// Add new obj to data array(newUser -> obj)
+const addData = obj => {
+  data.push(obj);
+  updateDOM(data);
+};
+
+// Update DOM
+const updateDOM = (proviedData = data) => {
+  // Clear the main div
+  main.innerHTML = '';
+
+  const h2 = document.createElement('h2');
+  const ul = document.createElement('ul');
+  ul.classList.add('standards');
+  ul.innerHTML = `<li>Person</li> <li>Age</li> <li>Country</li> <li>Wealth</li>`;
+  h2.appendChild(ul);
+  main.appendChild(h2);
+
+  proviedData.forEach(item => {
+    const element = document.createElement('li');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong><span>${item.age}</span><span>${item.location}</span><span>${item.money}</span>`;
+    main.appendChild(element);
+  });
+};
+
+addUserBtn.addEventListener('click', getRandomUser);
